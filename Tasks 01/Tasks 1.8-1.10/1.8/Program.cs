660366c19994b58aed098f7664470251f56bcd12
SimeonGavrilov <heavymetalfur@gmail.com>
2019-07-01 20:20:08 +0400

    Upload Tasks 1.8-1.10

---
+++ Tasks 01/Tasks 1.8-1.10/1.8/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Размерность массива");
+            string N = Console.ReadLine();
+            if (int.TryParse(N, out n)) n = int.Parse(N);
+            else
+            {
+                Console.WriteLine("Валидация провалилась!");
+                Console.ReadKey();
+                return;
+            }
+            int[,,] mass3D =new int[n,n,n];
+            Random r = new Random();
+            for(int i = 0; i<n;i++)
+            {
+                for(int j =0; j<n;j++)
+                {
+                    for(int k = 0; k<n;k++)
+                    {
+                        mass3D[i, j, k] = r.Next(-10, 5); 
+                    }
+                }
+            }
+            int[,] mass2D = new int[n,n];
+            for(int i = 0; i <n;i++)
+            {
+                for(int j = 0; j<n;j++)
+                {
+                    mass2D[i,j] = r.Next(-10,5);
+                }
+            }
+            Mass obj = new Mass();
+            Console.WriteLine("Вывод трёхмерного массива:");
+            obj.PrintMass3D(mass3D, n);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Сумма всех положительных элементов в 3D массиве равна {0}", obj.SumOfPositive(mass3D, n));
+            Console.WriteLine("Сумма всех чётных элементов в 2D массиве равна {0}", obj.SumOfEven(mass2D, n));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Произвожу обнуление всех положительных элементов... в 3D массиве");
+            obj.DoNull(mass3D, n);
+            obj.PrintMass3D(mass3D, n);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Произвожу вывод двумерного массива:");
+            obj.PrintMass2D(mass2D,n);
+            Console.ReadKey();
+        }
+        class Mass
+        {
+            public void PrintMass3D(int[,,] arr, int n) // просто вывод массива
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < n; k++)
+                        {
+                            Console.Write("{0} ", arr[i,j,k]);
+                        }
+                    }
+                }
+            }
+            public void PrintMass2D(int[,] arr, int n) // просто вывод массива
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < n; j++)
+                    {
+                            Console.Write("{0} ", arr[i, j]);
+                    }
+                }
+            }
+            public void DoNull(int[,,] arr, int n) //делаем нули из всех положительных значений
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            if(arr[i,j,k] >= 0) arr[i,j,k] = 0;
+                        }
+                    }
+                }
+            }
+            public int SumOfPositive(int[,,] arr, int n) //сумма положительных элементов массива
+            {
+                int summ = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            if (arr[i, j, k] > 0) summ += arr[i, j, k];
+                        }
+                    }
+                }
+                return summ;
+            }
+            public int SumOfEven(int[,] arr, int n) //сумма чётных элементов в массиве
+            {
+                int summ = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if ((i + j) % 2 == 0) summ += arr[i, j];
+                    }
+                }
+                return summ;
+            }
+        }
+    }
+}
---
+++ Tasks 01/Tasks 1.8-1.10/1.8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("1.8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1.8")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9b6e30cf-7512-445b-8b56-0d26032f68a9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
