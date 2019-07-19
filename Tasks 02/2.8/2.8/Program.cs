f2a6a205f49b5842f29b29e3eb186de38f26aad7
SimeonGavrilov <52416849+SimeonGavrilov@users.noreply.github.com>
2019-07-19 16:59:12 +0400

    Add files via upload

--- Tasks 02/2.8/2.8/Program.cs
+++ Tasks 02/2.8/2.8/Program.cs
@@ -4,271 +4,333 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2._8
+namespace _2._8_A1
 {
-public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            Player Player = new Player(5, 5, 20, 30, 15);
+            Mage_Spells Wrath = new Mage_Spells("Wrath", 5, 20);
+            Mage_Spells Healing_Touch = new Mage_Spells("Healing_Touch", 10, 20);
+            Rage_Spells Frenzy = new Rage_Spells("Frenzy", 50,60);
+            Player.Spell_Book.Add(Wrath);
+            Player.Spell_Book.Add(Healing_Touch);
+            Player.Spell_Book.Add(Frenzy);
+            Artefacts art = new Artefacts(10,10,50,50,50,"Любимая курочка Лироя",1);
+            Player.Invetory.Add(art);
+            foreach (var item in Player.Invetory)
+            {
+                Console.WriteLine(item);
+            }
+            //Paws.Active_Time = 100;
+            Console.ReadKey();
         }
     }
-    interface IMovable //интерфейс для движимых объектов (игрок, НИПы)
+    public abstract class Map
     {
-        void Move(int Move);
-    }
-    public class Map
-    {
-        protected int _X {get; set;}
-        protected int _Y {get; set;}
+        protected abstract int _X { get; set; }
+        protected abstract int _Y { get; set; }
         int[,] _Array;
         public Map(int X, int Y)
         {
-            _Array = new int[X,Y];
+            _Array = new int[X, Y];
             _X = X;
             _Y = Y;
         }
         public void Filling_Map()//метод заполнения массива
         {
             Random r = new Random();
-            for (int i = 0; i<_X; i++)
+            for (int i = 0; i < _X; i++)
             {
-                for(int j = 0; j<_Y; j++)
-                    _Array[i,j] = r.Next(0,2);
+                for (int j = 0; j < _Y; j++)
+                    _Array[i, j] = r.Next(0, 2);
             }
         }
         public void PrintMap()
         {
-            for (int i = 0; i<_X; i++)
+            for (int i = 0; i < _X; i++)
             {
                 Console.WriteLine();
-                for(int j = 0; j<_Y; j++)
-                    Console.Write("{0} ", _Array[i,j]);
+                for (int j = 0; j < _Y; j++)
+                    Console.Write("{0} ", _Array[i, j]);
             }
+        }
+        public bool Art()//проверка на наличие арта
+        {
+            if (_Array[_X, _Y] == 2) //крайне условно, допустим, значение 2 = наличию в клете артефакта. В этом случае возвращается истинное значение. Эдакая проверка на поднятие предмета. 
+            {
+                _Array[_X, _Y] = 0; //клетка обнуляется в пустую, но возможную для прохода.
+                return true;
+            }
+            else return false;
         }
     }
-    public abstract class Pawn //НИПы + игрок
+
+    interface IMovable //интерфейс для движимых объектов (игрок, НИПы)
     {
-        protected abstract int _X {get;set;}
-        protected abstract int _Y {get;set;}
-        public Pawn(int X, int Y)
+        void Move(int Move);
+    }
+    interface ILVLUpable //поднятие стат при поднятии бонуса.
+    {
+        void LVLUp();
+    }
+
+    public class Player : Map, IMovable, ILVLUpable
+    {
+        protected override int _X { get; set; } //координаты игрока по х и y
+        protected override int _Y { get; set; }
+        protected int _Stamina { get; set; } //выносливость
+        public virtual int Stamina
         {
-            _X=X; //Валидация граиц массива?
-            _Y=Y;
-        }
-        private int _Stamina {get; set;} //выносливость
-        public virtual int Stamina 
-        {
-            get {return _Stamina;}
+            get { return _Stamina; }
             set
             {
-                if(value < 0) throw new IndexOutOfRangeException("Значение выносливости не может быть отрицательным!");
-                            else _Stamina = value;
+                if (value <= 0) throw new ArgumentOutOfRangeException("Значение выносливости не может быть отрицательным!");
+                else _Stamina = value;
             }
         }
-        private int _Intellect {get; set;} //интеллект
+        protected int _Intellect { get; set; } //инта
         public virtual int Intellect
         {
-            get {return _Stamina;}
+            get { return _Intellect; }
             set
             {
-                if(value < 0) throw new IndexOutOfRangeException("Значение интеллекта не может быть отрицательным!");
-                            else _Intellect = value;
+                if (value <= 0) throw new ArgumentOutOfRangeException("Значение выносливости не может быть отрицательным!");
+                else _Intellect = value;
             }
         }
-        private int _Spirit {get; set;} //дух (просто третий стат для себя и примера)
+        protected int _Spirit { get; set; }//дух
         public virtual int Spirit
         {
-            get {return _Spirit;}
+            get { return _Spirit; }
             set
             {
-                if(value < 0) throw new IndexOutOfRangeException("Значение духа не может быть отрицательным!");
-                            else _Spirit = value;
+                if (value <= 0) throw new ArgumentOutOfRangeException("Значение выносливости не может быть отрицательным!");
+                else _Spirit = value;
             }
         }
-        private double _HP {get; set;}
-        public virtual double HP
+        private double _HP//устанавливается в зависимости от других параметров
         {
-            get {return (2 *_Stamina) + (0.5 * _Spirit);} //значение ХП не просто значение, а зависит от базовых стат.
+            get { return _Stamina * 2 + _Spirit * 1.2; }
+            private set { }
         }
-        private double _MP {get; set;}
-        public virtual double MP
+
+        private double _MP//устанавливается аналогично HP
         {
-            get {return (2 * _Intellect) + (0.5 * _Spirit);} // аналогично со значением Мана-пула
+            get { return _Intellect * 2 + _Spirit * 1.2; }
+            private set { }
         }
-    }
-    public class Player: Pawn, IMovable
-    {
-        protected override int _X {get; set;}
-        protected override int _Y {get; set;}
-        protected int _Stamina {get; set;}
-        protected int _Intellect {get; set;}
-        protected int _Spirit {get; set;}
-        protected int _HP {get; set;}
-        protected int _MP {get; set;}
-        
-        public Player(int X, int Y, int Stamina, int Intellect, int Spirit): base(X,Y)
+
+        public Player(int X, int Y, int Stamina, int Intellect, int Spirit)
+            : base(X, Y)
         {
-             if(Stamina < 0) throw new IndexOutOfRangeException("Значение выносливости не может быть отрицательным!");
-                 else _Stamina = Stamina;
-            
-             if(Stamina < 0) throw new IndexOutOfRangeException("Значение интеллекта не может быть отрицательным!");
-                 else _Intellect = Intellect;
-            
-             if(Stamina < 0) throw new IndexOutOfRangeException("Значение духа не может быть отрицательным!");
-                 else _Spirit = Spirit;
+            if (Stamina < 0) throw new IndexOutOfRangeException("Значение выносливости не может быть отрицательным!");
+            else _Stamina = Stamina;
+
+            if (Stamina < 0) throw new IndexOutOfRangeException("Значение интеллекта не может быть отрицательным!");
+            else _Intellect = Intellect;
+
+            if (Stamina < 0) throw new IndexOutOfRangeException("Значение духа не может быть отрицательным!");
+            else _Spirit = Spirit;
         }
         public void Move(int Move)
         {
             switch (Move)
             {
                 case 1:
-                //if(_Array[_X, _Y-1] != 1)
-                //{
-                //    _Array[_X,_Y] = 0;
-                //    _Array[_X, _Y-1] = 4;     Нужна проверка для движения. Т.е. если сверху непроходимая зона - движение отсутствует.
-                //}                             Аналогично для других кейсов, но я не буду там рассписывать для наглядности и простоты кода
-                //else
-                //{
-                //    break;
-                //}
-                Console.WriteLine("Двигаюсь вверх");
-                break;
-                
+                    //if(_Array[_X, _Y-1] != 1)
+                    //{
+                    //    _Array[_X,_Y] = 0;
+                    //    _Array[_X, _Y-1] = 4;     Нужна проверка для движения. Т.е. если сверху непроходимая зона - движение отсутствует.
+                    //}                             Аналогично для других кейсов, но я не буду там рассписывать для наглядности и простоты кода
+                    //else
+                    //{
+                    //    break;
+                    //}
+                    Console.WriteLine("Двигаюсь вверх");
+                    break;
+
                 case 2:
-                Console.WriteLine("Двигаюсь вниз");
-                break;
-                
+                    Console.WriteLine("Двигаюсь вниз");
+                    break;
+
                 case 3:
-                Console.WriteLine("Двигаюсь налево");
-                break;
-                
+                    Console.WriteLine("Двигаюсь налево");
+                    break;
+
                 case 4:
-                Console.WriteLine("Двигаюсь направо");
-                break;
+                    Console.WriteLine("Двигаюсь направо");
+                    break;
             }
-       }
-    }
-    public class NPC: Player, IMovable
-    {
-       protected int _X {get;set;}
-       protected int _Y {get;set;}
-       protected int _Stamina {get; set;}
-       protected int _Intellect {get; set;}
-       protected int _Spirit {get; set;}
-       protected double _HP {get; set;}
-       protected double _MP {get; set;}
-        
-        public NPC(int X, int Y, int Stamina, int Intellect, int Spirit): base(X,Y,Stamina, Intellect, Spirit)
+        }
+        public void LVLUp()
         {
-            
+            if (base.Art() == true) //если игрок находиться на одном уровне с артефактом - поднятие арта и, соответственно, бонуса
+            {
+                //Invetory.Add(/*Артефакт в данном месте*/); не знаю как реализовать
+                Intellect += 10; //некое поднятие уровня при поднятии бонуса.
+                Stamina += 10;
+            }
+        }
+        public List<Spells> Spell_Book = new List<Spells>();//тут все заклинания
+        public List<Artefacts> Invetory = new List<Artefacts>();//инвентарь
+    }
+    public class Artefacts : Player
+    {
+        protected override int _X { get; set; }
+        protected override int _Y { get; set; }
+        protected int _Stamina
+        {
+            get { return base._Stamina += _Stamina; }
+            private set { }
+        }
+        protected int _Intellect
+        {
+            get { return base._Intellect += _Intellect; }
+            private set { }
+        }
+        protected int _Spirit
+        {
+            get { return base._Spirit += _Spirit; }
+            private set { }
+        }
+        protected string _Name { get; set; }
+        public string Name
+        {
+            get { return _Name; }
+            private set { }
+        }
+        private int _ID { get; set; }
+        public Artefacts(int X, int Y, int Stamina, int Intellect, int Spirit, string Name, int ID)
+            : base(X, Y, Stamina, Intellect, Spirit)
+        {
+            _Name = Name;
+            _ID = ID;
+        }
+        public override string ToString()//метод перевода свойств класса artefacts в листе Invetory в строку. Крч, на выходе получаем что и сколько стоит.
+        {
+            return "X: " + _X + "   Y: " + _Y + " Stamina: " + Stamina + " Intellect: " + Intellect + " Spirit " + Spirit + " Name: " + Name + " ID: " + _ID;
         }
         
-        public void Move(int Move)
+    }
+    public class NPC : Player, IMovable
+    {
+        protected override int _X { get; set; }
+        protected override int _Y { get; set; }
+        private int _Stamina { get; set; }
+        private int _Intellect { get; set; }
+        private int _Spirit { get; set; }
+        private double _HP { get; set; }
+        private double _MP { get; set; }
+
+        public NPC(int X, int Y, int Stamina, int Intellect, int Spirit)
+            : base(X, Y, Stamina, Intellect, Spirit)
+        {
+
+        }
+
+        public new void Move(int Move) //для удаления ворнинга я поставил new
         {
             Random R = new Random();
-            Move = R.Next(1,5);
+            Move = R.Next(1, 5);
             switch (Move)
             {
                 case 1:
-                //if(_Array[_X, _Y-1] != 1)
-                //{
-                //    _Array[_X,_Y] = 0;
-                //    _Array[_X, _Y-1] = 4;     Нужна проверка для движения. Т.е. если сверху непроходимая зона - движение отсутствует.
-                //}                             Аналогично для других кейсов, но я не буду там рассписывать для наглядности и простоты кода
-                //else                          Движение для НИПов через рандом
-                //{
-                //    break;
-                //}
-                Console.WriteLine("Двигаюсь вверх");
-                break;
-                
+                    //if(_Array[_X, _Y-1] != 1)
+                    //{
+                    //    _Array[_X,_Y] = 0;
+                    //    _Array[_X, _Y-1] = 4;     Нужна проверка для движения. Т.е. если сверху непроходимая зона - движение отсутствует.
+                    //}                             Аналогично для других кейсов, но я не буду там рассписывать для наглядности и простоты кода
+                    //else                          Движение для НИПов через рандом
+                    //{
+                    //    break;
+                    //}
+                    Console.WriteLine("Двигаюсь вверх");
+                    break;
+
                 case 2:
-                Console.WriteLine("Двигаюсь вниз");
-                break;
-                
+                    Console.WriteLine("Двигаюсь вниз");
+                    break;
+
                 case 3:
-                Console.WriteLine("Двигаюсь налево");
-                break;
-                
+                    Console.WriteLine("Двигаюсь налево");
+                    break;
+
                 case 4:
-                Console.WriteLine("Двигаюсь направо");
-                break;
+                    Console.WriteLine("Двигаюсь направо");
+                    break;
             }
-       }
+        }
     }
-    public abstract class Objects: Map
-    {
-        protected abstract int _X {get;set;}
-        protected abstract int _Y {get;set;}
-    }
-    
-    interface IRemovable
+
+    interface IRemovable //интерфейс чисто для рассеивания заклинаний т.к. далеко не каждое можно снять. Это я вам как рестор друид говорю.
     {
         void Remove();
     }
-    
-    public abstract class Spells
+
+    public abstract class Spells //абстрактный класс заклинаний.
     {
-        protected double _Cost {get;set;} //цена спелла
+        protected abstract string _Name { get; set; } //название спелла
+        public virtual string Name
+        {
+            get { return _Name; }
+            private set { }
+        }
+        protected abstract double _Cost { get; set; } //цена каста
         public virtual double Cost
         {
-            get{return _Cost;}
-            private set{}
+            get { return _Cost; }
+            private set { }
         }
-        
-        protected double _Time_Of_Cust {get;set;} //время каста
-        public virtual double Time_Of_Cust
+        protected abstract double _Active_Time { get; set; }//время, которое спелл будет висеть
+        public virtual double Active_Time
         {
-            get{return _Time_Of_Cust;}
-            private set{}
+            get { return _Active_Time; }
+            private set { }
         }
-        
-        protected double _Cooldown {get;set;} //откат
-        public virtual double Cooldown
+        public Spells(string Name, double Cost, double Active_Time)
         {
-            get{return _Cooldown;}
-            private set{}
+            _Name = Name;
+            if (Cost >= 0 & Active_Time > 0)
+            {
+                _Cost = Cost;
+                _Active_Time = Active_Time;
+            }
+            else throw new IndexOutOfRangeException("Значение не может быть меньше нуля!");
         }
-        
-        public virtual double Heal_Power
+
+        public override string ToString()//метод перевода свойств класса spells в массиве Spell_Book в строку. Крч, на выходе получаем что и сколько стоит.
         {
-            get{return (2 * _Intellect) + (1.3 * _Spirit);}
-            private set{}
-        }
-        public virtual double Damage_Power
-        {
-            get { return (2 * _Intellect); }
-            private set{}
-        }
-        public Spells(double Cost, double Time_Of_Cust, double Cooldown)
-        {
-            if(Cost < 0) throw new IndexOutOfRangeException("Значение стоимости заклинания не может быть меньше нуля!");
-            else _Cost = Cost;
+            return "Name: " + Name + "   Cost: " + Cost + " Active Time: " + Active_Time;
         }
     }
-    public class Magic_Spells: Spells, IRemovable //Iremovable - можно рассеять. Класс обычных магических способностей
+    public class Mage_Spells : Spells, IRemovable
     {
-        private double _Cost;
-        private double _Time_Of_Cust;
-        private double _Cooldown;
-        private string _Shcool; //школа заклинания. Не знаю как в конструктор передать enum или List что-бы грамотно привязать тип
-        public Magic_Spells(double Cost, double Time_Of_Cust, double Cooldown, string School): base(Cost, Time_Of_Cust, Cooldown)
+        protected override string _Name { get; set; }
+        protected override double _Cost { get; set; }
+        protected override double _Active_Time { get; set; }
+        public Mage_Spells(string Name, double Cost, double Active_Time)
+            : base(Name, Cost, Active_Time)
         {
-            _Shcool = School;
+
         }
-        void Remove()
+        public void Remove() //диспел (рассеивание заклинаний)
         {
-            //Если на цель кастуется рассеивание - магические эффекты прекращают своё действие
+            _Active_Time = 0;
         }
     }
-    public class Rage_Spells: Spells //скиллы,которые работают не на магии, а на ярости (исступление и т.п.) рассеять невозможно
-    {                                // но при этом имеют те же параметры, что и обычные спеллы.
-        private double _Cost;
-        private double _Time_Of_Cust;
-        private double _Cooldown;
-        public Rage_Spells(double Cost, double Time_Of_Cust, double Cooldown): base(Cost, Time_Of_Cust, Cooldown)
+    public class Rage_Spells : Spells //разница просто в возможности рассеивания. Стоило ради этого создавать класс или можно было обойтись аналогичным булевским полем?
+    {
+        protected override string _Name { get; set; }
+        protected override double _Cost { get; set; }
+        protected override double _Active_Time { get; set; }
+        public Rage_Spells(string Name, double Cost, double Active_Time)
+            : base(Name, Cost, Active_Time)
         {
-            
+
         }
     }
 }
+
+
